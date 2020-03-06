6178e8bda78797ad078384267d2a6158b85550a2
langsm <791800537@qq.com>
2020-03-06 14:46:09 +0800

    3.6commit

---
+++ src/main/java/com/lsm/community/community/GithubProvider/GithubProvider.java
@@ -0,0 +1,47 @@
+package com.lsm.community.community.GithubProvider;
+
+import com.alibaba.fastjson.JSON;
+import com.lsm.community.community.dto.AccessTokenDTO;
+import com.lsm.community.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.sql.SQLOutput;
+
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+       MediaType mediaJSON = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+            RequestBody body = RequestBody.create(JSON.toJSONString(accessTokenDTO), mediaJSON);
+            Request request = new Request.Builder()
+                    .url("https://github.com/login/oauth/access_token")
+                    .post(body)
+                    .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string,GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
